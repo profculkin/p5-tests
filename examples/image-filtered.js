f5bf34ee3dbfd41ca767bd15ebaa43137df92eb9
profculkin <jculkin@bmcc.cuny.edu>
2017-11-19 16:29:14 -0500

    Add files via upload

--- examples/image-filtered.js
+++ examples/image-filtered.js
@@ -2,19 +2,19 @@ var img1, img2, img3;
 
 function preload() {
   img1 = loadImage("images/carousel.jpg");
-  img2 = loadImage("images/carousel.jpg");
-  img2 = loadImage("images/carousel.jpg");
+ /* img2 = loadImage("images/carousel.jpg");
+  img3 = loadImage("images/carousel.jpg");*/
 }
 
 function setup() {
- 	createCanvas(450, 150);   
+ 	 
     image(img1, 0, 0, 150, 150);
     
-    image(img2, 0, 150, 150, 150);
+    /*image(img2, 0, 150, 150, 150);
     filter(THRESHOLD);
     
     image(img3, 0, 300, 150, 150);
-    filter(GRAY);
+    filter(GRAY);*/
 }
 
 
