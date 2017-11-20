fe5f0d190da9f07d50f2109ed5086497409349ae
profculkin <jculkin@bmcc.cuny.edu>
2017-11-20 13:07:09 -0500

    Update video.js

--- examples/video.js
+++ examples/video.js
@@ -3,8 +3,8 @@ var playing = false;
 function setup() {
   createCanvas(960, 600);
   // specify multiple formats for different browsers
-  escalator = createVideo(['escalator.mov',
-                         'escalator.mp4']);
+  escalator = createVideo(['assets/escalator.mov',
+                         'assets/escalator.mp4']);
   escalator.hide(); // by default video shows up in separate dom
                   // element. hide it and draw it to the canvas
                   // instead
@@ -12,7 +12,7 @@ function setup() {
 
 function draw() {
   background(200);
-  image(escalator,10,c10, 192, 108); // draw the video frame to canvas
+  image(escalator,10, 10, 192, 108); // draw the video frame to canvas
   filter('GRAY');
   image(escalator,200,150, 640, 360); // draw a second copy to canvas
    text("Toggle videos on and off by clicking canvas", 10, 590);
