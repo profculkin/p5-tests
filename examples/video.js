30c3293c97139d82f7c23ec97a2c08ecbd365edb
profculkin <jculkin@bmcc.cuny.edu>
2017-11-20 12:41:44 -0500

    Add files via upload

---
+++ examples/video.js
@@ -0,0 +1,32 @@
+var escalator;
+var playing = false;
+function setup() {
+  createCanvas(960, 600);
+  // specify multiple formats for different browsers
+  escalator = createVideo(['escalator.mov',
+                         'escalator.mp4']);
+  escalator.hide(); // by default video shows up in separate dom
+                  // element. hide it and draw it to the canvas
+                  // instead
+}
+
+function draw() {
+  background(200);
+  image(escalator,10,10, 192, 108); // draw the video frame to canvas
+  filter('GRAY');
+  image(escalator,200,150); // draw a second copy to canvas
+   text("Toggle videos on and off by clicking canvas", 10, 590);
+}
+
+function mousePressed() {
+    if (playing === false){
+        playing = true;
+        // set the video to loop and start playing
+        escalator.loop();
+    } else if(playing === true){
+        //reset playing bariable and pause video
+        playing = false;
+        escalator.pause();
+    }  
+   // console.log(playing);
+}
